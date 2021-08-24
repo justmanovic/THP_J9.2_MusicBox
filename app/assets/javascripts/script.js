52dc0318f146dd934dc1ccbcfabe087d1c5d1474
jonathan justman <jojo.justman@gmail.com>
2021-08-24 18:15:53 +0200

    pourvu que ça marche

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,15 +1,15 @@
-let boom = document.querySelector("#audio81")
-let clap = document.querySelector("#audio83")
-let hihat = document.querySelector("#audio68")
-let kick = document.querySelector("#audio70")
-let openhat = document.querySelector("#audio71")
+let boom = document.getElementById("audio81")
+let clap = document.getElementById("audio83")
+let hihat = document.getElementById("audio68")
+let kick = document.getElementById("audio70")
+let openhat = document.getElementById("audio71")
 
 console.log('salut')
 
 document.addEventListener('keydown', (e) => {
   console.log('touche appuyée')
-  const audio = document.querySelector(`#audio${e.keyCode}`)
-  const key = document.querySelector(`#k${e.keyCode}`)
+  const audio = document.getElementById(`audio${e.keyCode}`)
+  const key = document.getElementById(`k${e.keyCode}`)
   if (!audio) return;
   audio.currentTime = 0;
   audio.play()
